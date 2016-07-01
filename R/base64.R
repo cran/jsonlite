1af9df72c337f35ed75964bcf8353b9888a12c53
Jeroen Ooms <jeroen.ooms@stat.ucla.edu>
2016-07-01 10:57:31 +0000

    version 1.0

--- R/base64.R
+++ R/base64.R
@@ -1,5 +1,18 @@
+#' Encode/decode base64
+#'
+#' Simple in-memory base64 encoder and decoder. Used internally for converting
+#' raw vectors to text. Interchangable with encoder from \code{base64enc} or
+#' \code{openssl} package.
+#'
+#' @param input string or raw vector to be encoded/decoded
+#' @export
+#' @rdname base64
+#' @name base64
 #' @useDynLib jsonlite R_base64_decode
-base64_decode <- function(input) {
+#' @examples str <- base64_enc(serialize(iris, NULL))
+#' out <- unserialize(base64_dec(str))
+#' stopifnot(identical(out, iris))
+base64_dec <- function(input) {
   if(is.character(input)){
     input <- charToRaw(paste(input, collapse = "\n"))
   }
@@ -7,13 +20,13 @@ base64_decode <- function(input) {
   .Call(R_base64_decode, input)
 }
 
-
+#' @export
+#' @rdname base64
 #' @useDynLib jsonlite R_base64_encode
-base64_encode <- function(input) {
+base64_enc <- function(input) {
   if(is.character(input)){
     input <- charToRaw(paste(input, collapse = "\n"))
   }
   stopifnot(is.raw(input))
-  buf <- .Call(R_base64_encode, input)
-  rawToChar(buf)
+  .Call(R_base64_encode, input)
 }
