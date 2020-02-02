55448562707f1471b8e74b9ae7b7edef47015123
Jeroen Ooms <jeroen@berkeley.edu>
2020-02-02 10:40:02 +0000

    version 1.6.1

--- src/is_datelist.c
+++ src/is_datelist.c
@@ -1,4 +1,5 @@
 #include <Rinternals.h>
+#include <string.h>
 
 //tests if all elements are either NULL or POSIXct, and at least one POSIXct
 SEXP C_is_datelist(SEXP x) {
@@ -11,6 +12,8 @@ SEXP C_is_datelist(SEXP x) {
   for (size_t i = 0; i < len; i++) {
     SEXP el = VECTOR_ELT(x, i);
     if(Rf_isNull(el))
+      continue;
+    if(Rf_isString(el) && Rf_length(el) > 0 && !strcmp(CHAR(STRING_ELT(el, 0)), "NA"))
       continue;
     if(Rf_isNumeric(el) && Rf_inherits(el, "POSIXct")){
       status = TRUE; //at least one date
